d33b45146ce7549b752f37ef83f85c422295df25
perkles <rfcompanhia@hotmail.com>
2022-03-20 14:00:34 -0300

    Commit Inicial

---
+++ AlunosApi/Controllers/AlunosController.cs
@@ -0,0 +1,18 @@
+﻿using AlunosApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlunosApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunosController : ControllerBase
+    {
+        private IAlunosService _alunosService;
+
+        public AlunosController(IAlunosService alunosService)
+        {
+            _alunosService = alunosService;
+        }
+    }
+}
