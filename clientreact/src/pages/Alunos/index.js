419c5724af290c10e695d6ddbcd4817e1d378c60
perkles <rfcompanhia@hotmail.com>
2022-03-25 21:25:44 -0300

    implementação de endpoints no frontend

--- clientreact/src/pages/Alunos/index.js
+++ clientreact/src/pages/Alunos/index.js
@@ -1,19 +1,62 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './styles.css';
+import api from '../../services/api';
 import logoCadastro from '../../assets/Cadastro.png';
 import iconLogout from '../../assets/Fechar.png';
 import iconEditar from '../../assets/Editar.png';
 import iconDeletar from '../../assets/Delete.png';
 
 export default function Alunos() {
+
+    const [nome, setNome] = useState('');
+    const [alunos, setAlunos] = useState([]);
+
+    const email = localStorage.getItem('email');
+    const token = localStorage.getItem('token');
+
+    const navigate = useNavigate()
+
+    const authorization = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    useEffect(() => {
+        api.get('api/alunos', authorization).then(
+            response => {
+                setAlunos(response.data);
+            }, token
+        )
+    })
+
+    async function logout(){
+        try{
+
+            localStorage.clear();
+            localStorage.setItem('token','');
+            authorization.headers='';
+            navigate('/')
+
+        }catch(err){
+            alert('Não foi possível fazer o logou' + err);
+        }
+    }
+
+    async function editAluno(id){
+        try{
+
+        }catch
+    }
+
     return (
         <div className='aluno-container'>
             <header>
                 <img src={logoCadastro} alt="Cadastro" id="cadastro" />
-                <span>Bem-Vindo, <strong>Raphael</strong></span>
+                <span>Bem-Vindo, <strong>{email}</strong></span>
                 <Link className='button' to="novo/0">Novo Aluno</Link>
-                <input type="image" src={iconLogout} alt="Submit" width="75" />
+                <input type="image" src={iconLogout} alt="Submit" width="75" onClick={logout}/>
             </header>
             <form>
                 <input type='text' placeholder="Nome" />
@@ -23,20 +66,16 @@ export default function Alunos() {
             </form>
             <h1>Relação de Alunos</h1>
             <ul>
-                <li>
-                    <b>Nome: </b>Paulo<br /><br />
-                    <b>Email: </b>paulo@email.com<br /><br />
-                    <b>Idade: </b>22<br /><br />
-                    <input type="image" src={iconEditar} alt="Submit" width="45" />
-                    <input type="image" src={iconDeletar} alt="Submit" width="45" />
-                </li>
-                <li>
-                    <b>Nome: </b>Paulo<br /><br />
-                    <b>Email: </b>paulo@email.com<br /><br />
-                    <b>Idade: </b>22<br /><br />
-                    <input type="image" src={iconEditar} alt="Submit" width="45" />
-                    <input type="image" src={iconDeletar} alt="Submit" width="45" />
-                </li>
+                {alunos.map(aluno => (
+                    <li key={aluno.id}>
+                        <b>Nome: </b>{aluno.nome}<br /><br />
+                        <b>Email: </b>{aluno.email}<br /><br />
+                        <b>Idade: </b>{aluno.idade}<br /><br />
+
+                        <input type="image" src={iconEditar} alt="Submit" width="45" />
+                        <input type="image" src={iconDeletar} alt="Submit" width="45" />
+                    </li>
+                ))}
             </ul>
         </div>
     );
--- clientreact/src/pages/NovoAluno/index.js
+++ clientreact/src/pages/NovoAluno/index.js
@@ -1,12 +1,54 @@
-import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import './styles.css';
 import iconAdicionar from '../../assets/Adicionar.png';
 import iconRetornar from '../../assets/Retornar.png';
+import api from '../../services/api';
+
 
 export default function NovoAluno() {
 
+    const [id,setId] = useState(null);
+    const [nome,setNome] = useState('');
+    const [email,setEmail] = useState('null');
+    const [idade,setIdade] = useState(0);
+
     const {alunoId} = useParams();
+
+    const navigate = useNavigate()
+
+    const token = localStorage.getItem('token');
+
+    const authorization = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    useEffect(() => {
+       if(alunoId === '0')
+            return;
+
+        else    
+            loadAluno();
+
+    },alunoId)
+
+    async function loadAluno(){
+        try{
+
+            const response = await api.get(`api/alunos/${alunoId}`, authorization);
+
+            setId(response.data.id);
+            setNome(response.data.nome);
+            setEmail(response.data.email);
+            setIdade(response.data.idade);
+
+        }catch(error){
+            alert('Erro ao recuperar o aluno' + error);
+            navigate('/alunos');
+        }
+    }
 
     return (
         <div className='novo-aluno-container'>
@@ -19,9 +61,18 @@ export default function NovoAluno() {
                 </Link>
                 </section>
                 <form>
-                    <input placeholder='Nome' />
-                    <input placeholder='Email' />
-                    <input placeholder='Idade' />
+                    <input placeholder='Nome' 
+                        value={nome}
+                        onChange={e=> setNome(e.target.value)}
+                    />
+                    <input placeholder='Email' 
+                        value={email}
+                        onChange={e=> setEmail(e.target.value)}
+                    />
+                    <input placeholder='Idade' 
+                        value={idade}
+                        onChange={e=> setIdade(e.target.value)}
+                    />
                     <button className='button' type='submit'>{alunoId === '0' ? 'Incluir' : 'Atualizar'}</button>
                 </form>
             </div>     
